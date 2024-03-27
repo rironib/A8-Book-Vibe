004f9b724a76215e767c9d60ae60adc67762edb9
Razibul Islam <dev.ronib@gmail.com>
2024-03-27 12:17:02 +0600

    Dist folder removed

--- src/Components/BookLists/BookLists.jsx
+++ src/Components/BookLists/BookLists.jsx
@@ -29,7 +29,15 @@ const BookLists = () => {
     return (
         <div>
             <div className='bg-[#1313130D] p-8 font-bold text-3xl text-center mb-8 rounded-2xl'>Books</div>
+
             <div className='text-center mb-6'>
+                <select className="bg-green-500 focus:bg-white select select-bordered w-42 max-w-xs">
+                    <option disabled selected>Sort By</option>
+                    <option>Rating</option>
+                    <option>Published Year</option>
+                    <option>Number of Pages</option>
+                </select>
+
                 <div className="dropdown">
                     <div tabIndex="0" role="button" className="btn m-1">Sort By</div>
                     <ul tabIndex="0" className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
@@ -39,8 +47,21 @@ const BookLists = () => {
                     </ul>
                 </div>
             </div>
+
+            <form className="max-w-sm mx-auto">
+                <select id="countries"
+                        className=" border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    <option selected>Choose a country</option>
+                    <option value="US">United States</option>
+                    <option value="CA">Canada</option>
+                    <option value="FR">France</option>
+                    <option value="DE">Germany</option>
+                </select>
+            </form>
+
             <div role="tablist" className="tabs tabs-lifted">
-                <input onChange={handleChange} type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Read Books" checked/>
+                <input onChange={handleChange} type="radio" name="my_tabs_2" role="tab" className="tab"
+                       aria-label="Read Books" checked/>
                 <div role="tabpanel" className="tab-content border-t-base-300 py-6">
                     {
                         dispalyReadLists.map((book) => <ListedBook key={book.bookId} book={book}/>)
